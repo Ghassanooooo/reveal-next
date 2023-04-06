4c3ca880eb6c541a91f2dd244e3ab25a67578506
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2023-04-06 17:03:20 +0200

    ready to DO

--- utils/baseURL.ts
+++ utils/baseURL.ts
@@ -1,6 +1,11 @@
-const baseURL = "http://localhost:4444";
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://reveal.crtil.com"
+    : "http://localhost:4444";
 // new port for client 4111 nginx
 // new port for client 4111 nginx
+
+console.log("ENV => ", process.env.NODE_ENV);
 
 export const presentationApi = baseURL + "/presentation-api/trpc";
 export const socketApi = baseURL;
